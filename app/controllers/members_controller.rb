b8a9e1c5d4ee46b744289a1abb148a26a7d684e1
skuan <stanykuan@gmail.com>
2014-08-08 17:13:58 -0700

    recent upload with mindeventsorg and pw in fields

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,7 +3,7 @@ class MembersController < ApplicationController
 	def home
 		
 		# Hardworker.perform_async('bob', 5) ignore this line... was trying to setup automatic updater/worker
-				session = GoogleDrive.login("", "")
+				session = GoogleDrive.login(ENV["mindeventsorg@gmail.com"], ENV["events2012"])
 				ws = session.spreadsheet_by_key("1q8CzSMk2ro_AbEf_oEBkHSr5bVYL2jzKMI2GmbWzCZE").worksheets[0]
 				ws.reload
 
@@ -19,26 +19,4 @@ class MembersController < ApplicationController
 	end
 				
 				
-				
-
-					# @b2 = p ws[2,2]
-					# @c2 = p ws[2,3]
-					# @d2 = p ws[2,4]
-					# @e2 = p ws[2,5]
-					# @f2 = p ws[2,6]
-					# @g2 = p ws[2,7]
-					# @h2 = p ws[2,8]
-					# @i2 = p ws[2,9]
-
-					# @b3 = p ws[3,2]
-					# @c3 = p ws[3,3]
-					# @d3 = p ws[3,4]
-					# @e3 = p ws[3,5]
-					# @f3 = p ws[3,6]
-					# @g3 = p ws[3,7]
-					# @h3 = p ws[3,8]
-					# @i3 = p ws[3,9]
-
-	
-
 end
